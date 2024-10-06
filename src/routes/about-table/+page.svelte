45c124198b4f86f2ace8decb54ac995cdb4129a8
YuneKomori <yune.komori0103art@gmail.com>
2024-10-06 22:00:19 +0900

    initial commit

---
+++ src/routes/about-table/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { Table } from '@skeletonlabs/skeleton';
+  
+    // ヘッダーとデータ行の正しい形式を定義
+    const tableData = {
+      head: ['Header 1', 'Header 2', 'Header 3'], // ヘッダーを配列として定義
+      body: [
+        ['Row 1, Cell 1', 'Row 1, Cell 2', 'Row 1, Cell 3'],  // 各行のセルを配列として定義
+        ['Row 2, Cell 1', 'Row 2, Cell 2', 'Row 2, Cell 3']
+      ]
+    };
+  </script>
+  
+  <Table class="striped" source={tableData} />
+  
+  
