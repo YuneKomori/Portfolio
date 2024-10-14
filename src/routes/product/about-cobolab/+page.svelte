32c11eaeac08c543efe66276d7573ab5ee933a27
YuneKomori <yune.komori0103art@gmail.com>
2024-10-14 17:46:00 +0900

    詳細ページを設計しました

---
+++ src/routes/product/about-cobolab/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+
+
+</script>
+
+<h2 class="mb-10 mt-14 max-w-2xl px-4 text-2xl font-extrabold leading-none dark:text-white"
+    data-svelte-h="svelte-1t4n0i2">登壇資料</h2>
+
+<style>
+
+</style>
