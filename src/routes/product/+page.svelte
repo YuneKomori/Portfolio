c79cc3da8ec8d2017ef5fd1022675b1850e88dc1
YuneKomori <yune.komori0103art@gmail.com>
2024-10-15 01:40:58 +0900

    デプロイの設定をしました

--- src/routes/product/+page.svelte
+++ src/routes/product/+page.svelte
@@ -4,11 +4,11 @@
 </script>
 
 <div>
-    <section id="features" class="bg-surface-100-800-token features-gradient svelte-1fg2m4z">
+    <section id="features" class=" features-gradient svelte-1fg2m4z">
         <div class="section-container svelte-1fg2m4z">
             <div class="space-y-4 md:space-y-10">
                 <div class="space-y-4 text-center" data-svelte-h="svelte-1jpjkss">
-                    <h2 class="h2">My Product</h2>
+                    <h2 class="h2  mt-8">My Product</h2>
                     <p>今までに制作したプロダクトの一覧ページです。</p>
                 </div>
                 <div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
